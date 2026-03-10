af2ee3b4df2ef7f58483b85ed74ff63905b9e455
zarlox41 <zarlox41@gmail.com>
2026-03-09 20:36:51 -0500

    deploy render

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://TU_BACKEND_URL/api'; // <- cambiar por la URL de Render o localhost:4000/api
+const API_BASE = 'https://project-fullstack-backend-nbdj.onrender.com'; // <- cambiar por la URL de Render o localhost:4000/api
 
 // --- Helper ---
 function setToken(token) { localStorage.setItem('token', token); }
